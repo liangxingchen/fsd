c036f6ad9f525e9ad53a3a5c3d9a7ecc1bc9d286
ZhaoLei <zhao@maichong.it>
2018-02-08 15:08:01 +0800

    reader

---
+++ test/cases/readdir.js
@@ -0,0 +1,29 @@
+import test from 'tape';
+import type { fsd as fsdFn } from 'fsd';
+
+export default function (fsd: fsdFn) {
+  test('readdir', (troot) => {
+    test('mkdir abc', async(t) => {
+      let dir = fsd('/abc');
+      await dir.mkdir();
+      t.ok(await dir.exists(), 'mkdir error');
+      t.end();
+    });
+
+    test('readdir abc', async(t) => {
+      let dir = fsd('/abc');
+      await dir.readdir(true);
+      t.ok(await dir.readdir(true), 'readdir OK');
+      t.end();
+    });
+
+    test('clear', async(t) => {
+      let dir = fsd('/abc');
+      if (await dir.exists()) {
+        await dir.unlink();
+      }
+      t.end();
+    });
+    troot.end();
+  });
+}
